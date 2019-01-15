51682b640ce836b6a4defb4a8849221c540b9672
Adrian Hartanto <adrian.hartanto@traveloka.com>
2019-01-15 11:43:21 +0700

    Add missing prop-types

--- components/CoachmarkView.js
+++ components/CoachmarkView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CoachmarkContent from './CoachmarkContent';
 import CoachmarkArrow, { K_POSITION_TOP, K_POSITION_BOTTOM } from './CoachmarkArrow';
 
